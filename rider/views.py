1ab0c15f1b1c1b20ca1398411e6b441a0b1581f5
Anjum -aforrogit config --global user.email <anjumdnadaf27@gmail.com>
2026-09-04 18:30:27 +0530

    updated git api

--- rider/views.py
+++ rider/views.py
@@ -1,18 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Rider
 from .serializers import RiderSerializer
 
 
 class RiderListView(APIView):
+  def get(self, request):
+    riders = Rider.objects.all()
 
-    def get(self, request):
-        rider = Rider.objects.all()
-        serializer = RiderSerializer(rider, many=True)
+    search = request.GET.get("search")
 
-        return Response({
-            "status": True,
-            "data": serializer.data,
-            "message": "Riders retrieved successfully"
-        })
+    if search:
+        riders = riders.filter(name__icontains=search)
+
+    vehicle_number = request.GET.get("vehicle_number")
+
+    if vehicle_number:
+        riders = riders.filter(vehicle_number__icontains=vehicle_number)
+
+    page = int(request.GET.get("page", 1))
+    page_size = int(request.GET.get("page_size", 10))
+
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    total_count = riders.count()
+    riders = riders[start:end]
+
+    serializer = RiderSerializer(riders, many=True)
+
+    return Response({
+        "status": True,
+        "data": serializer.data,
+        "page": page,
+        "page_size": page_size,
+        "total_count": total_count,
+        "message": "Riders retrieved successfully"
+    })
